e0bb6b476ceb2b2a190891a390931511cc0a8e66
baker-snowflaker <117956963+baker-snowflaker@users.noreply.github.com>
2022-11-15 13:04:35 +1100

    Test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
-  streamliut.dataframe(my_data_rows)
+  streamlit.dataframe(my_data_rows)
 
 
 def insert_row_snowflake(new_fruit):
